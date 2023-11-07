8dd0c81e31dfcb3498ae17acf99183fa8de6afc2
Raffael <mail@raffael-vogler.de>
2023-11-07 13:34:35 +0100

    mühsam ernährt sich das eichhörnchen

--- ginrummy.pl
+++ ginrummy.pl
@@ -1,27 +1,41 @@
-% [3/h, 4/h, 5/h, 9/d, 10/d, 11/d, 12/d, 4/d, 4/c, 4/s]
+comb_all([],[]).
 
-% http://kti.ms.mff.cuni.cz/~bartak/prolog/combinatorics.html
+comb_all([X|T],[X|Comb]) :-
+    comb_all(T,Comb).
 
-comb(0,_,[]).
+comb_all([_|T],Comb) :-
+    comb_all(T,Comb).
 
-comb(N,[X|T],[X|Comb]) :- 
+sum_pts([],0).
+
+sum_pts([C|T],S) :-
+    sum(T,S_),
+    (C =< 10 -> C_ = C ; C_ = 10),
+    S is C_ + S_.
+
+comb_n(0,_,[]).
+
+comb_n(N,[X|T],[X|Comb]) :-
     N>0,
     N1 is N-1,
-    comb(N1,T,Comb).
+    comb_n(N1,T,Comb).
 
-comb(N,[_|T],Comb) :-
+comb_n(N,[_|T],Comb) :-
     N>0,
-    comb(N,T,Comb).
+    comb_n(N,T,Comb).
 
 sets_of_3(X, S) :-
-    comb(3, X, S),
+    comb_n(3, X, S),
     [A/_,B/_,C/_] = S,
     A = B, B = C.
 
 runs_of_3(X, R) :-
     sort(X,X2),
-    comb(3,X2,R),
+    comb_n(3,X2,R),
     [R1/S1,R2/S2,R3/S3] = R,
     S1 = S2, S2 = S3,
     1 is R3 - R2,
     1 is R2 - R1.
+
+% cards_of_sets(X, C) :-
+% findall(C,sets_of_3([1/a,1/b,1/c,2/x,2/d,3/c,3/b,3/a,1/d],C),Cs),comb_all(Cs,S),union(S,U).
